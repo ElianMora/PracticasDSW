c258ba9fc5366c9704c8fa243753a2e79bd6e832
AlumnoFCA <elian.nuno@uabc.edu.mx>
2023-10-10 07:37:21 -1000

    Practica IF comparar 2 numeros

---
+++ practica26/ejercicio26.js
@@ -0,0 +1,12 @@
+var num1 = 5; 
+var num2 = 8; 
+
+if(num1 <= num2){
+    console.log("num1 no es mayor que num 2");
+}
+if(num2 >= 0){
+    console.log("num2 es positivo")
+}
+if(++num1 < num2){
+    console.log("incrementa uno en num1, no lo hace mayor que num2")
+}
